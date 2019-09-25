26f5c81872b09e5f3a105a8ed1f57f61723eb348
awaheed <awaheed@utexas.edu>
2019-09-25 11:18:22 -0500

    updated connection file with databse name

--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,7 @@ var connetion = require("../config/connection.js")
 
 var orm = {
     selectAll: function (tableInput, cb) {
-        var sqlString = "SELECT * FROM burgers_db." + tableInput + ";"
+        var sqlString = "SELECT * FROM k8ahkxbxycfvglph." + tableInput + ";"
         connetion.query(sqlString, function (err, result) {
             if (err) throw err;
             cb(result)
